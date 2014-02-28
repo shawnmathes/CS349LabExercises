4cf9c23e35df5d6efad05a9c6caa73055e3c1845
Shawn Mathes <shawnmathes@gmail.com>
2014-02-27 18:19:32 -0600

    Added menu.

--- src/edu/umkc/smbr5/cs349/project1/PhotoAlbum.java
+++ src/edu/umkc/smbr5/cs349/project1/PhotoAlbum.java
@@ -1,7 +1,14 @@
 package edu.umkc.smbr5.cs349.project1;
 
 import java.awt.BorderLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+
 import javax.swing.JFrame;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
 
 @SuppressWarnings("serial")
 public class PhotoAlbum extends JFrame {
@@ -10,7 +17,28 @@ public class PhotoAlbum extends JFrame {
     PagerPanel pagerPanel = new PagerPanel();
     
     public PhotoAlbum() {
-        super("Photo Album");        
+        super("Photo Album");
+        
+        JMenuBar menuBar = new JMenuBar();
+        setJMenuBar(menuBar);
+        
+       // Create the first menu.
+        JMenu menu = new JMenu("File");
+        menu.setMnemonic(KeyEvent.VK_F);
+        menuBar.add(menu);
+
+        // Create an item for the first menu
+        JMenuItem exitMenuItem = new JMenuItem("Exit",KeyEvent.VK_X);
+        exitMenuItem.addActionListener(new ActionListener () {
+
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                System.exit(0);
+            }});
+
+        //menuItem.setMnemonic(KeyEvent.VK_X); // set in constructor
+        menu.add(exitMenuItem);
+        
         setLayout(new BorderLayout());
         add(photoPanel, BorderLayout.CENTER);
         add(pagerPanel, BorderLayout.SOUTH);
